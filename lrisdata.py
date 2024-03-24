e8db15fbd9966377b0ffaf068c6bf0a21750dcfc
maulana damar <111202214620@mhs.dinus.ac.id>
2024-03-24 15:15:55 +0700

    UPload project machine learning

---
+++ lrisdata.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import pandas as pd
+
+df = pd.read_csv('iris.csv')
+
+
+# In[5]:
+
+
+df.head()
+
+
+# In[6]:
+
+
+df.info()
+
