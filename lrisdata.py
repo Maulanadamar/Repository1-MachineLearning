ab3d66ca112a0a9c19af869b6be967b7d5a26405
Maulana Damar Adhesyah Putra <113022357+Maulanadamar@users.noreply.github.com>
2024-04-14 18:24:51 +0700

    Add files via upload

--- lrisdata.py
+++ lrisdata.py
@@ -1,28 +1,84 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[37]:
 
 
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_selection import f_regression
+from sklearn.feature_selection import SelectKBest
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+
+import matplotlib.pyplot as plt
 
 
-# In[3]:
+# In[16]:
 
 
-import pandas as pd
-
-df = pd.read_csv('iris.csv')
+iris_data = pd.read_csv("Iris.csv")
 
 
-# In[5]:
+# In[17]:
 
 
-df.head()
+X = iris_data.drop('Species', axis=1)  
+y = iris_data['Species']                      
 
 
-# In[6]:
+# In[24]:
 
 
-df.info()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# In[27]:
+
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+# In[30]:
+
+
+svm_model = SVC(kernel='linear', random_state=42)
+
+
+# In[31]:
+
+
+svm_model.fit(X_train, y_train)
+
+
+# In[32]:
+
+
+y_pred = svm_model.predict(X_test)
+
+
+# In[35]:
+
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Akurasi:", accuracy)
+
+
+# In[38]:
+
+
+print("\nLaporan Klasifikasi:")
+print(classification_report(y_test, y_pred))
+
+
+# In[ ]:
+
+
+
 
